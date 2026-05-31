74465a7166d76f79ba8522e1a3c5999d33d640c6
RohitKumarYadav3 <rohityadav75769@gmail.com>
2026-05-31 13:05:14 +0530

    utils changed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,3 @@
-type ClassValue = string | undefined | null | boolean | ClassValue[];
-
-export function cn(...inputs: ClassValue[]): string {
-  return inputs
-    .flat(Infinity)
-    .filter((x): x is string => typeof x === "string" && x.length > 0)
-    .join(" ");
+export function cn(...inputs: (string | undefined | null | false)[]): string {
+  return inputs.filter(Boolean).join(" ");
 }
